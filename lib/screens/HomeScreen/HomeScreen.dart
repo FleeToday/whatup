23a0b7a10a5bf8bdab6638569204a2a75ac5f42c
brotherko <ko19950314@gmail.com>
2019-12-19 22:49:14 +0800

    scroll horizontal

--- lib/screens/HomeScreen/HomeScreen.dart
+++ lib/screens/HomeScreen/HomeScreen.dart
@@ -39,6 +39,7 @@ class HomeScreen extends StatelessWidget {
 
   Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     return ListView(
+      scrollDirection: Axis.horizontal,
       padding: const EdgeInsets.only(top: 20.0),
       children: snapshot.map((data) => _buildListItem(context, data)).toList(),
     );
