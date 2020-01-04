a82a42af7841e303aa90f1cc1db30320d8d04a6d
brotherko <ko19950314@gmail.com>
2020-01-04 23:37:04 +0800

    new component for create activity

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:whatup/screens/LoginScreen/LoginScreen.dart';
-import 'screens/HomeScreen/HomeScreen.dart';
-import 'screens/ActivityCreateScreen/ActivityCreateScreen.dart';
+import 'package:whatup/screens/HomeScreen/LocationSearchPopupWidget.dart';
+import 'package:whatup/screens/HomeScreen/HomeScreen.dart';
+import 'package:whatup/screens/ActivityCreateScreen/ActivityCreateScreen.dart';
 
-// import 'package:example/screens/example2/examplescreen2.dart';
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/login": (BuildContext context) => LoginScreen(),
   "/": (BuildContext context) => HomeScreen(),
+  "/search_location": (BuildContext context) => LocationSearchPopupWidget(),
   "/create": (BuildContext context) => ActivityCreateScreen(),
 };
