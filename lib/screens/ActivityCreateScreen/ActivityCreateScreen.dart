67a82d4ccd38dac5e2f0ded94276f119c28b4476
brotherko <ko19950314@gmail.com>
2020-01-06 20:02:20 +0800

    basic add activity

--- lib/screens/ActivityCreateScreen/ActivityCreateScreen.dart
+++ lib/screens/ActivityCreateScreen/ActivityCreateScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:whatup/models/Activity.dart';
+import 'package:whatup/resources/Repository.dart';
 
 class ActivityCreateScreen extends StatefulWidget {
   @override
@@ -7,7 +9,15 @@ class ActivityCreateScreen extends StatefulWidget {
 }
 
 class _ActivityCreateScreenState extends State<ActivityCreateScreen> {
+  final _activity = Activity();
   final _formKey = GlobalKey<FormState>();
+  Repository _repository = Repository();
+
+  void _formSubmit() {
+    _formKey.currentState.save();
+    _repository.addActivity(_activity);
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +34,26 @@ class _ActivityCreateScreenState extends State<ActivityCreateScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            TextFormField(),
-            TextFormField(),
+            TextFormField(
+              decoration: InputDecoration(hintText: "Title"),
+              onSaved: (String val) {
+                setState(() {
+                  _activity.title = val;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(hintText: "Description"),
+              onSaved: (String val) {
+                setState(() {
+                  _activity.description = val;
+                });
+              },
+            ),
             Padding(
                 padding: const EdgeInsets.all(10),
                 child: RaisedButton(
+                  onPressed: _formSubmit,
                   child: Text("submit"),
                 ))
           ],
