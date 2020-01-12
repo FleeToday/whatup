f6eec06400191865addedfba82876d447fc5ee23
Nelson Cheung <cubecnelson@gmail.com>
2020-01-12 15:49:40 +0800

    set theme data

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,15 @@ class MyApp extends StatelessWidget {
       title: 'WhatUp',
       initialRoute: '/login',
       routes: routes,
+      // theme: new ThemeData(
+      //   brightness: Brightness.light,
+      //   primaryColor: Colors.white,
+      //   accentColor: Colors.orange[200],
+      //   backgroundColor: Colors.orange[200],
+      //   buttonColor: Colors.orange[200],
+      //   primaryTextTheme: TextTheme(),
+      //   indicatorColor: Colors.orange[200],
+      // ),
     );
   }
 }
