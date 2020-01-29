8ee3a830d38d5e0548d06ea1daa884820071d95b
brotherko <ko19950314@gmail.com>
2020-01-29 23:44:24 +0800

    new swiper style

--- lib/models/Activity.dart
+++ lib/models/Activity.dart
@@ -5,6 +5,8 @@ class Activity {
   String title;
   String description;
   LatLng location;
+  String locationName;
+  DateTime datetime;
   DocumentReference reference;
   GeoPoint _;
 
@@ -12,11 +14,15 @@ class Activity {
   Activity.fromMap(Map<String, dynamic> map, {this.reference})
       : assert(map['title'] != null),
         assert(map['description'] != null),
+        assert(map['location_name'] != null),
         assert(map['location'] != null),
+        assert(map['datetime'] != null),
         title = map['title'],
         description = map['description'],
         location = LatLng((map['location']['geopoint'] as GeoPoint).latitude,
-            (map['location']['geopoint'] as GeoPoint).longitude);
+            (map['location']['geopoint'] as GeoPoint).longitude),
+        locationName = map['location_name'],
+        datetime = map['datetime'].toDate();
 
   Activity.fromSnapshot(DocumentSnapshot snapshot)
       : this.fromMap(snapshot.data, reference: snapshot.reference);
